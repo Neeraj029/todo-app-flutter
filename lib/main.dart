a9d5e785afb510681a9cb8479eee539f590adeb5
Neerajsamshette <neeraj.samshette@gmail.com>
2021-05-08 11:47:03 +0530

    firebase app not created

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async => {
       WidgetsFlutterBinding.ensureInitialized(),
-      Firebase.initializeApp(),
+      await Firebase.initializeApp(),
       runApp(MaterialApp(home: MyApp()))
     };
 
